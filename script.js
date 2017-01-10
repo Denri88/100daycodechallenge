b3b51846274bd26da3a5cb7b58b325911884ff07
denri88 <clatx88@gmail.com>
2017-01-09 21:21:07 -0600

    added js guessing game.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("site under construction. :) ")
+alert("you have 10 guesses. Guess 1-10");
+
+var answer = 3;
+var guess = prompt("What's your guess?");
+
+for (var i = 0;i <= 10; i++) {
+	
+	if(answer == guess) {
+		alert("You guessed correctly!");
+		break;
+	} else {
+		guess = prompt("please try again.");
+	}
+}
